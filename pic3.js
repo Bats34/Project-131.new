c1d7d4d97f648ab73e8c5fe26c153696b782a120
Sanvi <96112662+Bats34@users.noreply.github.com>
2023-05-01 10:38:01 +0530

    Add files via upload

---
+++ pic3.js
@@ -0,0 +1,21 @@
+img="";
+status="";
+
+function preload() {
+    imgz=loadImage('picture_3.jpg');
+
+}
+function setup() {
+    canvas=createCanvas(500,430);
+    canvas.position(480,230);
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model has loaded!!");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function draw() {
+    image(imgz,0,0,500,430);
+}
